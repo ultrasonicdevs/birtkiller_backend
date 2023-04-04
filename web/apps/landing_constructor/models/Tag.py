b4f7f2fb20c9ee6d60736e518cba06cc926c255e
lev sviridov <levgeneral@yandex.ru>
2023-04-04 20:41:48 +0300

    added swagger added nullable to style and otherprops tag fields

--- web/apps/landing_constructor/models/Tag.py
+++ web/apps/landing_constructor/models/Tag.py
@@ -6,6 +6,6 @@ class Tag(models.Model):
     tagname = models.CharField(max_length=128)
     component_type = models.CharField(max_length=128)
     parent = models.ForeignKey('self', related_name='children', null=True, on_delete=models.CASCADE)
-    style = models.JSONField(blank=True)
-    other_props = models.JSONField(blank=True)
+    style = models.JSONField(blank=True, null=True)
+    other_props = models.JSONField(blank=True, null=True)
     page = models.ForeignKey(Page, blank=False, null=False, related_name='tags', on_delete=models.CASCADE)
